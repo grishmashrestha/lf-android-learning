c6f6bb8769d833291b6122b9e3f62b00f2d7a1bd
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-02-11 16:18:14 +0545

    validation tasks move to Validator Class from MyActivity class

--- app/src/main/java/com/example/grishma/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/grishma/myfirstapp/MyActivity.java
@@ -2,20 +2,23 @@ package com.example.grishma.myfirstapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+
+import com.example.grishma.myfirstapp.util.Validator;
 
 import java.util.HashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+
+/**
+ * Shows user login form
+ */
 
 public class MyActivity extends AppCompatActivity {
     @Bind(R.id.txtUsername) EditText username;
@@ -58,11 +61,12 @@ public class MyActivity extends AppCompatActivity {
         String usernameString = username.getText().toString();
         String passwordString = password.getText().toString();
         boolean isBothValid;
+        Validator validator = new Validator();
 
-        if (!validateUsername(usernameString)) {
+        if (!validator.validateUsername(usernameString)) {
             isBothValid = false;
             username.setError("invalid username");
-        } else if (!validatePassword(passwordString)) {
+        } else if (!validator.validatePassword(passwordString)) {
             isBothValid = false;
             password.setError("invalid password");
         } else {
@@ -75,21 +79,5 @@ public class MyActivity extends AppCompatActivity {
             intent.putExtra("hashMap", hashMap);
             startActivity(intent);
         }
-    }
-
-    private Boolean validateUsername(String username) {
-        boolean flag = false;
-        if (username.length() <= 10) {
-            flag = true;
-        }
-        return flag;
-    }
-
-    private Boolean validatePassword(String password) {
-        boolean flag = false;
-        if (password.length() == 4) {
-            flag = true;
-        }
-        return flag;
     }
 }
