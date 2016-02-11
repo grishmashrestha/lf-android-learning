705b69cedbd07d8eec00c06725824b562414746e
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-02-11 10:27:17 +0545

    removed floating icon of message from app

--- app/src/main/java/com/example/grishma/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/grishma/myfirstapp/MyActivity.java
@@ -28,15 +28,6 @@ public class MyActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ButterKnife.bind(this);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
--- app/src/main/java/com/example/grishma/myfirstapp/WelcomeActivity.java
+++ app/src/main/java/com/example/grishma/myfirstapp/WelcomeActivity.java
@@ -22,15 +22,6 @@ public class WelcomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         Intent intent = getIntent();
 
         HashMap<String, String> hashMap = (HashMap<String, String>) intent.getSerializableExtra("hashMap");
