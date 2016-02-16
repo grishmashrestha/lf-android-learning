6bc160246060ad64676e3866de610809879a395d
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-02-16 14:52:05 +0545

    model user created

---
+++ app/src/main/java/com/example/grishma/myfirstapp/model/User.java
@@ -0,0 +1,25 @@
+package com.example.grishma.myfirstapp.model;
+
+/**
+ * Created by grishma on 2/16/16.
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
